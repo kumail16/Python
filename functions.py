c76811f629c6abfa8e573108af6143acb61459a2
Kumail Abbas <kumail.abbas1689@gmail.com>
2022-07-13 17:21:21 +0500

    some progress

---
+++ functions.py
@@ -0,0 +1,8 @@
+def myfunction(fruit):
+    print('Line 1 ' + fruit)
+    print('Line 2 ' + fruit)
+    print('Line 3 ' + fruit)
+
+myfunction('apple')
+myfunction('banana')
+myfunction('water melon')
---
+++ word Length.py
@@ -0,0 +1,2 @@
+word = input('Enter the word: ')
+print ('The word ' + word + ' has ' +str(len(word)) + ' letters in it')
