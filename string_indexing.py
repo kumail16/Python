fb451f13769a259197465b0ff6ca2a56d440fbc6
Kumail Abbas <kumail.abbas1689@gmail.com>
2022-07-15 13:00:49 +0500

    Update string_indexing.py

--- string_indexing.py
+++ string_indexing.py
@@ -1,4 +1,4 @@
-user_input = input('Enter a word: ')
+user_input = input('Enter a word and I will tell you its last letter: ')
 final_index = len(user_input) - 1
 last_character = user_input[final_index]
 last_character = user_input[-1]
