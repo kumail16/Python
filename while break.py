5895870c7931d56e59a3b6141d9ff9c63fab7199
Kumail Abbas <kumail.abbas1689@gmail.com>
2023-07-06 11:36:37 +0500

    Update while break.py

--- while break.py
+++ while break.py
@@ -1,10 +1,19 @@
 while True:
     print('Who are you?')
+    
     name = input()
+    
     if name != 'Kumail':
+        print('Only Kumail is allowed')
         continue
+    
     print('Hello Kumail, What is the password?')
+    
     password = input()
+    
     if password == 'cisco':
         break
+    elif password !='cisco':
+        print('Wrong Password')
+        
 print('''Access Granted''')
